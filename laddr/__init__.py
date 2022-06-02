df8dcad14197abaf80e760f5f1649a085fa9eb04
antoniouaa <antoniouaa@hotmail.com>
2022-06-02 23:19:58 +0100

    pyproj toml created and main deps installed

---
+++ laddr/__init__.py
@@ -0,0 +1,5 @@
+"""Career tracking app for contact information"""
+
+__version__ = "0.1.0"
+
+from laddr.app import create_app as create_app
---
+++ laddr/app.py
@@ -0,0 +1,22 @@
+from venv import CORE_VENV_DEPS
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def create_app():
+    app = FastAPI()
+    origins = ["http://localhost", "http://localhost:3000"]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    @app.get("/")
+    async def index():
+        return {"hello": "world"}
+
+    return app
